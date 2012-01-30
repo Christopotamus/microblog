04e23af24158f671092789d88a37c7e74e53f99d
Christopher Budd <chris.cyborg@gmail.com>
2012-01-29 16:12:06 -0800

    Logins working using sessions.

--- microblogapp/urls.py
+++ microblogapp/urls.py
@@ -3,8 +3,8 @@ from microblogapp.views import *
 
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home),
+    url(r'^login/', login),
+    url(r'^logout/', logout),
 )
--- microblogapp/views.py
+++ microblogapp/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
+from wuphf.models import *
+import hashlib
 
 def home(request):
+    args = {}
+    if "login_failed" in request.session:
+        print request.session['login_failed']
+        args = {"login_failed":request.session['login_failed']}
     if "user" in request.session:
-        print request.session['user']
+        return render_to_response('index.html', {"user":request.session['user']},context_instance=RequestContext(request))
     else:
-        request.session['user'] = 'Chris'
+        return render_to_response('login.html', args,context_instance=RequestContext(request))
 
-    return render_to_response('index.html', '',context_instance=RequestContext(request))
+def login(request):
+    if 'username' in request.POST and 'password' in request.POST:
+        if request.POST['username'] != '' and request.POST['password'] != '':
+            username = request.POST['username']
+            password = hashlib.md5(request.POST['password']).hexdigest()
+            try:
+                user = auth_users.objects.get(username=username, password=password)
+            except auth_users.DoesNotExist:
+                request.session['login_failed'] = True
+                return redirect('/')
+            else:
+                request.session['user'] = user.username
+                request.session['login_failed'] = False 
+                return redirect('/')
+
+    request.session['login_failed'] = True
+    return redirect('/')
+
+def logout(request):
+    if "user" in request.session:
+        del request.session['user']
+
+    return redirect('/')
